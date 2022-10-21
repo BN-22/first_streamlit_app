838611d6151d94d7e576eb34452270fffbf0b433
BN-22 <116361922+BN-22@users.noreply.github.com>
2022-10-21 16:06:27 +0200

    Update streamlit_app.py
    fixed the position of the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as sl
 import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.dataframe(my_fruit_list)
-
 sl.title("My Mom's New Healthy Diner")
 
 sl.header('Breakfast Favorites')
@@ -13,3 +10,6 @@ sl.text('🐔 Hard-Boiled Free-Range Eggs')
 sl.text('🥑🍞 Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
